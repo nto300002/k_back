0e9cbb85846594928dda1b88cc71e1345ab60540
yasuda <samonkntd@gmail.com>
2025-08-03 16:39:53 +0900

    add/schemas: office schema

--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -1,7 +1,7 @@
 from .staff import StaffCreate, StaffUpdate, StaffResponse, StaffInviteRequest
 from .token import Token
 from .msg import Msg
-from .office import OfficeCreateRequest, OfficeResponse
+from .office import OfficeCreate, OfficeResponse
 # from .recipient import RecipientCreate, RecipientUpdate, RecipientResponse
 # from .support_plan import (
 #     SupportPlanCycleResponse,
