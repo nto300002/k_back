2aa504726f055f4017fa0ec8e7b7a2a6aac738d9
yasuda <samonkntd@gmail.com>
2025-08-03 16:45:33 +0900

    add/api: office

--- app/api/v1/api.py
+++ app/api/v1/api.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
-from .endpoints import auth, staff, offices
+from .endpoints import office # auth, staff, 
 
 api_router = APIRouter()
-api_router.include_router(auth.router, prefix="/auth", tags=["Auth"])
-api_router.include_router(staff.router, prefix="/staff", tags=["Staff"])
-api_router.include_router(offices.router, prefix="/offices", tags=["Offices"])
+# api_router.include_router(auth.router, prefix="/auth", tags=["Auth"])
+# api_router.include_router(staff.router, prefix="/staff", tags=["Staff"])
+api_router.include_router(office.router, prefix="/office", tags=["Offices"])  
